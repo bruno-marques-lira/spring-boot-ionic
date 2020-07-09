5668af7d20837c8a6780239e14388d7828ac4588
Bruno Lira <brunomarqueslira.one@gmail.com>
2020-07-09 15:51:59 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/brnmlira/cursomc/services/PedidoService.java
+++ src/main/java/com/brnmlira/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.brnmlira.cursomc.domain.Cliente;
 import com.brnmlira.cursomc.domain.ItemPedido;
 import com.brnmlira.cursomc.domain.PagamentoComBoleto;
 import com.brnmlira.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.brnmlira.cursomc.domain.enums.EstadoPagamento;
 import com.brnmlira.cursomc.repositories.ItemPedidoRepository;
 import com.brnmlira.cursomc.repositories.PagamentoRepository;
 import com.brnmlira.cursomc.repositories.PedidoRepository;
+import com.brnmlira.cursomc.security.UserSS;
+import com.brnmlira.cursomc.services.exceptions.AuthorizationException;
 import com.brnmlira.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -74,5 +80,15 @@ public class PedidoService {
 	public void delete(Integer id) {
 		find(id);
 		repo.deleteById(id);
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
